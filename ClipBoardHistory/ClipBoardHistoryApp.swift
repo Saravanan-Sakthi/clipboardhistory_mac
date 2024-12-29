47008263c51a60a818fe3a406135dd2c0a335a63
Saravanan C <saravanan.cn@zohocorp.com>
2024-12-29 11:31:02 +0530

    Initial Commit

---
+++ ClipBoardHistory/ClipBoardHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipBoardHistoryApp.swift
+//  ClipBoardHistory
+//
+//  Created by Saravanan C on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipBoardHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipBoardHistoryTests/ClipBoardHistoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipBoardHistoryTests.swift
+//  ClipBoardHistoryTests
+//
+//  Created by Saravanan C on 29/12/24.
+//
+
+import Testing
+@testable import ClipBoardHistory
+
+struct ClipBoardHistoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
